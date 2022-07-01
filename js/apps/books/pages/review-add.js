c01a0e7f45d01655420400a860713afe1c3a49bc
RonElbaz <RonElbaz@users.noreply.github.com>
2022-07-01 17:19:09 +0300

    Books Commit
    integrated books

--- js/apps/books/pages/review-add.js
+++ js/apps/books/pages/review-add.js
@@ -1,81 +1,66 @@
 import { bookService } from "../service/book-service.js";
 
 export default {
-  props: ['id'],
+  props:["id"],
+    template: `
+    <h1>Reviews</h1>
+    <div v-if="pastReviews" v-for="review in pastReviews">
+          <pre>{{review}}</pre>
+     </div>
 
-  template: ` 
-  <h1>Review</h1>
-   <form @submit.prevent="saveReview">
-      <input ref="nameInput" type="text" v-model="name" >
-      <br>
-      <form class="rating">
-  <label>
-    <input type="radio" name="stars" value="1" v-model="rating" />
-    <span class="icon">★</span>
-  </label>
-  <label>
-    <input type="radio" name="stars" value="2"  v-model="rating" />
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-  </label>
-  <label>
-    <input type="radio" name="stars" value="3"  v-model="rating" />
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-    <span class="icon">★</span>   
-  </label>
-  <label>
-    <input type="radio" name="stars" value="4"  v-model="rating" />
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-  </label>
-  <label>
-    <input type="radio" name="stars" value="5"  v-model="rating" />
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-    <span class="icon">★</span>
-  </label>
-</form>
-<br>
-<input type="date" v-model="currDate">
-<br>
-<textarea v-model="review" cols="30" rows="10"></textarea>
-<br>
-<input type="submit" value="submit">
-</form>
-<section v-if="id">
-  <ul v-if="book.reviews"></ul>
-  
-   </section>
-   `
-  ,
-  data() {
-    return {
-      name: "Books Reader",
-      rating: 0,
-      currDate: Date.now(),
-      review: '',
+     <h1>Add Review</h1>
+     <form @submit="saveRev">
+        <input ref="nameInput" type="text" v-model="review.name" >
+        <select v-model.number="review.rating" name="rating" id="rating">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <input v-model="review.date" type="date" >
+          <textarea v-model="review.freeText" rows="4" cols="50"></textarea>
+          <br/>
+          <input type="submit" value="Submit">
+     </form>
+     <!-- <pre>{{name}}</pre>
+     <pre>{{rating}}</pre>
+     <pre>{{date}}</pre>
+     <pre>{{freeText}}</pre>
+     <pre>{{id}}</pre> -->
 
 
-    }
-  },
-  mounted() {
-    this.$refs.nameInput.focus()
-  },
-  methods: {
-    saveReview() {
-      bookService.addReview(this.id, { name: this.name, reting: this.rating, currDate: this.currDate, review: this.review })
-    }
-  },
-  created() { },
-  computed: {
-    getCurrDate() {
-      return Date.now()
-    }
-  },
-  components: {},
+  `,
+    data() {return{
+
+      review:{
+        name: "Books Reader",
+        rating:1,
+        date:null,
+        freeText:""
+
+      },
+      pastReviews: null,
+
+    }},
+    mounted(){
+      this.$refs.nameInput.focus()
+      let day = new Date().getDate() < '10' ?  '0' + new Date().getDate() : new Date().getDate()
+      let month = new Date().getMonth() < '10' ?  '0' + (+new Date().getMonth()+1) : (+new Date().getMonth()+1)
+      this.review.date = new Date().getFullYear() + "-" + month  + "-" + day
+      bookService.get(this.id).then(book => this.pastReviews = book.reviews)
+    },
+    methods: {
+      saveRev(){
+        bookService.addReview(this.id, {...this.review}).then(()=>{
+
+          bookService.get(this.id).then(book => this.pastReviews = book.reviews)
+        })
+      }
+    },
+    created(){
+    },
+    computed: {
+    },
+    components: {},
 };
